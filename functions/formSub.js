cbfe4f872c8d50cb5be71fac972fba9899905dc9
Mvrcus <marcuswilson.22@gmail.com>
2024-02-21 23:27:29 -0800

    json response

--- functions/formSub.js
+++ functions/formSub.js
@@ -1,22 +1,26 @@
-export async function onRequest(context) {
-  // Retrieve the form data from the request
-  const { name, email, message } = await context.request.json();
-
-  // Access the database connection from the environment variable
-  const database = context.env.formDB;
-
-  // Prepare an SQL query to insert the form data into a table
-  // Make sure the table and column names match your database schema
-  const query = `INSERT INTO portfolioDB (name, email, message) VALUES (?, ?, ?)`;
-  const ps = database.prepare(query);
-
-  // Execute the query with the form data
+export async function onRequestPost(context) {
   try {
-    const result = await ps.run(name, email, message);
-    return new Response('Form submitted successfully', { status: 200 });
-  } catch (error) {
-    // Handle any errors that occur during the database operation
-    console.error('Database error:', error);
-    return new Response('Error submitting form', { status: 500 });
+    let input = await context.request.formData();
+
+    // Convert FormData to JSON
+    // NOTE: Allows multiple values per key
+    let output = {};
+    for (let [key, value] of input) {
+      let tmp = output[key];
+      if (tmp === undefined) {
+        output[key] = value;
+      } else {
+        output[key] = [].concat(tmp, value);
+      }
+    }
+
+    let pretty = JSON.stringify(output, null, 2);
+    return new Response(pretty, {
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+    });
+  } catch (err) {
+    return new Response('Error parsing JSON content', { status: 400 });
   }
 }
