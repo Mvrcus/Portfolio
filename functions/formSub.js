bdd25e1292c5ae2125b5c4e9e3127ed10530bbb4
Mvrcus <marcuswilson.22@gmail.com>
2024-02-24 00:36:21 -0800

    test formsub

--- functions/formSub.js
+++ functions/formSub.js
@@ -1,27 +1,30 @@
-export async function onRequest(request) {
-    try {
-      const { name, email, message } = await request.json();
-  
-      // Perform any processing or validation here with the received data
-  
-      // For now, let's log the received data and return a response
-      console.log('Received data:', { name, email, message });
-  
-      return new Response('Form data received successfully!', {
-        status: 200,
-        headers: {
-          'Content-Type': 'text/plain',
-        },
-      });
-    } catch (error) {
-      console.error('Error processing form data:', error);
-  
-      return new Response('Error processing form data', {
-        status: 500,
-        headers: {
-          'Content-Type': 'text/plain',
-        },
-      });
+/**
+ * POST /api/submit
+ */
+export async function onRequestPost(context) {
+  try {
+    // Convert FormData to JSON
+    // NOTE: Allows multiple values per key
+    let output = {};
+    for (let [key, value] of context.request.formData()) {
+      let tmp = output[key];
+      if (tmp === undefined) {
+        output[key] = value;
+      } else {
+        output[key] = [].concat(tmp, value);
+      }
     }
+
+    // Perform any processing or validation here with the received data
+    console.log('Received data:', output);
+
+    // Return a response
+    return new Response(JSON.stringify(output, null, 2), {
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+      },
+    });
+  } catch (err) {
+    return new Response('Error parsing JSON content', { status: 400 });
   }
-  
+}
