5734da8202622b166449e1dac7b1d0770db77628
Mvrcus <marcuswilson.22@gmail.com>
2024-02-24 16:13:58 -0800

    https://marcustwilson.com/form/submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -167,7 +167,7 @@ style="fill:#40C057;">
 
   // Make an asynchronous request to your Cloudflare Workers function
   try {
-    const response = await fetch('/form/submit', {
+    const response = await fetch('https://marcustwilson.com/form/submit', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
