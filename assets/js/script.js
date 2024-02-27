c4dc9f63c132a2baefc606f0d47b087cbcb8ccf9
Mvrcus <marcuswilson.22@gmail.com>
2024-02-26 23:49:49 -0800

    remove onload, remove unused custom field

--- assets/js/script.js
+++ assets/js/script.js
@@ -388,26 +388,23 @@ document.addEventListener('DOMContentLoaded', function () {
     // Hide sections
     document.querySelector('.mapbox').style.display = 'none';
     document.querySelector('.contact-form').style.display = 'none';
-    window.onload = function () {
-      // Add message in a new section
-      const messageSection = document.createElement('section');
-      messageSection.className = 'form-success';
-      messageSection.innerHTML = `<div class="confetti-wrapper">
+    // Add message in a new section
+    const messageSection = document.createElement('section');
+    messageSection.className = 'form-success';
+    messageSection.innerHTML = `<div class="confetti-wrapper">
   <svg class="success-icon" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256"
 style="fill:#40C057;">
 <g fill="#40c057" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><g transform="scale(8.53333,8.53333)"><path d="M15,3c-6.627,0 -12,5.373 -12,12c0,6.627 5.373,12 12,12c6.627,0 12,-5.373 12,-12c0,-2.17938 -0.59,-4.21686 -1.60547,-5.97852l-11.24805,11.24609c-0.187,0.187 -0.44103,0.29297 -0.70703,0.29297c-0.265,0 -0.52003,-0.10497 -0.70703,-0.29297l-4.45313,-4.45312c-0.391,-0.391 -0.391,-1.02306 0,-1.41406c0.391,-0.391 1.02306,-0.391 1.41406,0l3.74609,3.74609l10.80078,-10.80078c-2.201,-2.655 -5.52223,-4.3457 -9.24023,-4.3457zM24.24023,7.3457c0.43165,0.52058 0.81351,1.08435 1.1543,1.67383l2.3125,-2.3125c0.391,-0.392 0.391,-1.02306 0,-1.41406c-0.391,-0.391 -1.02306,-0.391 -1.41406,0z"></path></g></g>
 </svg>
-</div><p>Hello, ${fullName}! Thank you for submitting the contact form.</p>`;
+</div><p>Thank you for submitting the contact form.</p>`;
 
-      const contactFormSection = document.querySelector('.contact-form');
-      contactFormSection.parentNode.insertBefore(
-        messageSection,
-        contactFormSection.nextSibling
-      );
-
-      // Trigger the confetti
-      loadParticles(configs);
-    };
+    const contactFormSection = document.querySelector('.contact-form');
+    contactFormSection.parentNode.insertBefore(
+      messageSection,
+      contactFormSection.nextSibling
+    );
+    // Trigger the confetti
+    loadParticles(configs);
   }
 });
 
